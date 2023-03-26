3b1011b00d5049fea73a2655e193ee4497eb922c
inderjeet.s.bamrah <inderjeet.s.bamrah@gmail.com>
2023-03-26 16:55:25 +0530

    Billing Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Button = ({ styles }) => {
-  return <button type="button"></button>;
+  return (
+    <button
+      type="button"
+      className={`bg-blue-gradient font-poppins py-4 px-6 font-medium text-primary text-[18px] outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  );
 };
 
 export default Button;
